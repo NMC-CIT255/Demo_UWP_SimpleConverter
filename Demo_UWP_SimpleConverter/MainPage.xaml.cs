e03b2328c92e63f8fc0c8be6b76fcbab3e9586fd
John Velis <john@johnvelis.com>
2016-11-09 05:38:57 -0500

    radio added

---
+++ Demo_UWP_SimpleConverter/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Demo_UWP_SimpleConverter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void SpeakButton_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(ConvertedValueTextBox.Text);
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            ConvertedValueTextBox.Text = (int.Parse(ValueToCovertTextBox.Text) * 12).ToString();
+        }
+    }
+}
---
+++ Demo_UWP_SimpleConverter_02/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Demo_UWP_SimpleConverter
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void SpeakButton_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(ConvertedValueTextBox.Text);
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
+
+
+        private void InchesToFeetRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (ValueToCovertTextBox.Text != "")
+            {
+                ConvertedValueTextBox.Text = (double.Parse(ValueToCovertTextBox.Text) / 12).ToString("0.00");
+            }
+        }
+
+        private void FeetToInchesRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (ValueToCovertTextBox.Text != "")
+            {
+                ConvertedValueTextBox.Text = (double.Parse(ValueToCovertTextBox.Text) * 12).ToString("0.00");
+            }
+        }
+    }
+}
